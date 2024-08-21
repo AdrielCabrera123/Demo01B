a602104f4ac27982c47b684c0cfb2eb19a73616d
Alumno <Alumno@LAB1507-17>
2024-08-20 19:51:18 -0500

    Agregar archivos de proyecto.

---
+++ Demo01B/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Persona persona = new Persona();
+            persona.Nombres = "Carlos Adriel";
+            persona.Apellidos = "Cabrera Ricalde";
+            persona.AsignarNombresCompletos();
+            
+
+
+
+            Triangulo triangulo = new Triangulo();
+            triangulo.Base = 4;
+            triangulo.Altura = 5;
+            Console.WriteLine(triangulo.CalcularArea());
+
+
+
+            Console.WriteLine(persona.NombresCompletos);
+            Console.WriteLine("Hola Mundo");
+            Console.Read();
+        }
+    }
+}
